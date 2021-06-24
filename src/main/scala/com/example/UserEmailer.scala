953ab4c221c72da372c87f5a0cb363424c3291f6
Jack Wells <jack939@tpg.com.au>
2021-06-24 20:44:01 +1000

    Cleaning Repository

--- src/main/scala/com/example/UserEmailer.scala
+++ src/main/scala/com/example/UserEmailer.scala
@@ -12,12 +12,14 @@ object UserEmailer {
     trait Service {
         def notify(user: User, message: String): Task[Unit]
     }
+
     //Service Implementation
     val live: ZLayer[Any, Nothing, UserEmailerEnv] = ZLayer.succeed(new Service {
         override def notify(user: User, message: String) = Task {
             println(s"[User Emailer] Sending '$message' to ${user.email}")
         }
     })
+    
     //Service Front-facing API
     def notify(user: User, message: String): ZIO[UserEmailerEnv, Throwable, Unit] =
         ZIO.accessM(hasService => hasService.get.notify(user,message))
--- src/main/scala/com/example/UserSubscription.scala
+++ src/main/scala/com/example/UserSubscription.scala
@@ -16,10 +16,12 @@ object UserSubscription {
                 _ <- notifier.notify(user, s"Welcome to the world, ${user.name}! We have some nice ZIO content for you.")
             } yield user
     }
+
     //Service Implementation
     val live = ZLayer.fromServices[UserEmailer.Service, UserDb.Service, UserSubscription.Service] {
         (userEmailer, userDb) => new Service(userEmailer, userDb)
     }
+    
     //Service Front-facing API
     def subscribe(user: User): ZIO[UserSubscriptionEnv, Throwable, User] =
         ZIO.accessM(_.get.subscribe(user))
